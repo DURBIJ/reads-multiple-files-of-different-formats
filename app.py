dd8740ab8b2d9e1c3b95c08317ced30361c1534f
DURBIJ <durbijkewat@gmail.com>
2024-05-05 14:18:20 +0530

    Added support for multiple files of different formats

--- app.py
+++ app.py
@@ -42,6 +42,13 @@ def extract_text(file):
     content = ""
     if file.filename.endswith('.txt'):
         content = file.read().decode('utf-8')
+    elif file.filename.endswith('.csv'):
+        content = file.read().decode('utf-8')
+    elif file.filename.endswith('.pdf'):
+        content = textract.process(file)
+    elif file.filename.endswith(('.doc', '.docx', '.ppt', '.pptx')):
+        content = textract.process(file, method='pdftotext')  
+    
     return content
 
 
